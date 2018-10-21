1c4937a088a2a9022c72f6786d8b95cf3167cd6d
Rafael Freire <rafaelfreireweb@gmail.com>
2018-10-21 20:14:48 -0300

    Updates

--- precache-manifest.4fc5bec4bd3b41e1947a3f4dcdba95c0.js
+++ precache-manifest.4fc5bec4bd3b41e1947a3f4dcdba95c0.js
@@ -32,12 +32,12 @@ self.__precacheManifest = [
     "url": "/react-crud/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/react-crud/static/media/fontawesome-webfont.674f50d2.eot"
-  },
-  {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/react-crud/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-crud/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-crud/precache-manifest.814c6b2c106d97eb636aecc4bf7798c3.js"
+  "/react-crud/precache-manifest.4fc5bec4bd3b41e1947a3f4dcdba95c0.js"
 );
 
 workbox.clientsClaim();
