cc2b3ab52412bcf283ff710d41c39f0fe382a1d4
Rafael Freire <rafaelfreireweb@gmail.com>
2018-10-21 20:31:38 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-crud/precache-manifest.d7983c931c5129deb00917305d0ef373.js"
+  "/react-crud/precache-manifest.814c6b2c106d97eb636aecc4bf7798c3.js"
 );
 
 workbox.clientsClaim();
