c24f4f2dbcc27e03f7de01296fa52b7b075f8632
Rafael Freire <rafaelfreireweb@gmail.com>
2018-10-21 19:18:12 -0300

    Updates

---
+++ precache-manifest.307c03fa3cce46871dddef533ef557c7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e30f4db590d46c735276abc4fd4f5dd4",
+    "url": "/react-crud/static/media/logo.e30f4db5.png"
+  },
+  {
+    "revision": "fe307fc7b9c75e9cccf9",
+    "url": "/react-crud/static/css/main.ac7e8112.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-crud/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f9c64d80b59a7c599dff",
+    "url": "/react-crud/static/js/1.f9c64d80.chunk.js"
+  },
+  {
+    "revision": "66e01a8977a9d2cce54a",
+    "url": "/react-crud/static/js/runtime~main.66e01a89.js"
+  },
+  {
+    "revision": "cad0d684c482f904a17259854da480e9",
+    "url": "/react-crud/static/media/preload.cad0d684.gif"
+  },
+  {
+    "revision": "fe307fc7b9c75e9cccf9",
+    "url": "/react-crud/static/js/main.fe307fc7.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-crud/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-crud/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-crud/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-crud/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "f9c64d80b59a7c599dff",
+    "url": "/react-crud/static/css/1.122065ab.chunk.css"
+  },
+  {
+    "revision": "4170487eb89661987508870465d571d8",
+    "url": "/react-crud/index.html"
+  }
+];
